7386888f210533fb72a12adab7a3f8a2553dce64
Doris Lee <dorislee@berkeley.edu>
2015-10-25 16:51:02 -0700

    debugged gridsearch script

--- grid_search.py
+++ grid_search.py
@@ -2,6 +2,7 @@ import numpy as np
 import yt
 import sklearn
 yt.funcs.mylog.setLevel(50)
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.cross_validation import cross_val_score
 from sklearn.cluster import KMeans
@@ -12,8 +13,7 @@ def debug(s,n=""):
 	print s
 	print n
 debug("Loading Particle Data")
-prefix = "http://darksky.slac.stanford.edu/scivis2015/data/ds14_scivis_0128/"
-ds = yt.load(prefix+"ds14_scivis_0128_e4_dt04_1.0000")
+ds = yt.load("../ds14_scivis_0128_e4_dt04_1.0000")
 ad = ds.all_data()
 x = ad[("all","particle_position_x")]
 y = ad[("all","particle_position_y")]
@@ -21,8 +21,8 @@ z = ad[("all","particle_position_z")]
 debug("Creating train test split samples")
 train = []
 test = []
-N = 10#50
-N_split = 2#10
+N = 20#50
+N_split = 4#10
 for n in np.arange(N):
     if n >N_split:
 #         train.append(list(get_halo(filtered_catalog)[-1][n][0])[:6])
@@ -50,8 +50,11 @@ debug(grid)
 debug("Conducting hyperparameter grid search")
 grid.fit(train, labels)
 debug("Writing out grid scores and plotting")
-np.savetxt(grid.grid_scores_)
-plt.plot(k_range, grid_mean_scores)
-plt.xlabel("number of clusters",fontsize=13)
-plt.ylabel("cross validated  accuracy",fontsize=13)
-plt.savefig("accuracy.png")
+info =[]
+scores=[]
+for i in grid.grid_scores_:
+    info.append([i[0]["n_clusters"], i[1], i[2].std()])
+    #so i[2] is the list of results from cross validating 10 times (K folds, K=10)
+    scores.append(i[2])
+np.savetxt("info.txt",info,header="n_clusters         mean_score      std_score")
+np.savetxt("scores.txt",scores)
