c6d6494406db32b3e28189d843c196b5367c69a0
Doris Lee <dorislee@berkeley.edu>
2015-11-08 19:01:54 -0800

    killed sclaling test large, trying a smaller one

--- getscript.py
+++ getscript.py
@@ -7,10 +7,11 @@ if nproc<24:
     print "#PBS -l mppwidth=24"
 else:
     print "#PBS -l mppwidth={}".format((nproc/24)*24)
-print "#PBS -l walltime=03:00:00"
+print "#PBS -l walltime=10:00:00"
 print "#PBS -N {}_test".format(nproc)
 print "#PBS -e sctest.$PBS_JOBID.err"
 print "#PBS -o sctest.$PBS_JOBID.out"
 print "#PBS -A m2218"
-print "cd /project/projectdirs/astro250/doris/halo/halo_find/test{}".format(nproc)
+print "export LD_LIBRARY_PATH=/global/homes/d/dorislee/anaconda/lib:$LD_LIBRARY_PATH"
+print "cd /project/projectdirs/astro250/doris/halo/halo_find/2test{}".format(nproc)
 print "aprun -n 1 /global/homes/d/dorislee/anaconda/bin/python parallel_gs_dens_test.py" 
